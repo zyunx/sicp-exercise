eee2ba803c7ffbfd21f821d2b8c813b6f3aa34d4
zyunx <zyunx@zyunx.net>
2017-09-16 00:38:38 +0800

    3.69

--- common/stream.scm
+++ common/stream.scm
@@ -11,3 +11,16 @@
   (cons-stream n (integers-start-at (+ n 1))))
 
 (define integers (integers-start-at 1))
+
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (interleave s2 (stream-cdr s1)))))
+
+
+(define (stream-flatmap proc s)
+  (let ((first-stream (proc (stream-car s))))
+    (cons-stream (stream-car first-stream)
+                 (interleave (stream-cdr first-stream)
+                             (stream-flatmap proc (stream-cdr s))))))
