b68619057ac1871bb88b94fd1362cd51c2ec1d79
zyunx <zyunx@zyunx.net>
2017-12-17 16:59:19 +0800

    Exercise 4.5

--- example/4.1/4.1.2.scm
+++ example/4.1/4.1.2.scm
@@ -140,17 +140,22 @@
   (expand-clauses (cond-clauses exp)))
 ;Value: cond->if
 
+(define (arrow-action? actions) (eq? '=> (car actions)))
+
 (define (expand-clauses clauses)
   (if (null? clauses)
       'false
       (let ((first (car clauses))
-	    (rest (cdr clauses)))
-	(if (cond-else-clause? first)
+	        (rest (cdr clauses)))
+	    (if (cond-else-clause? first)
             (if (null? rest)
-	        (sequence->exp (cond-actions first))
-	        (error "ELSE clause isn't last -- COND->IF" clauses))
-	    (make-if (cond-predicate first)
-                     (sequence->exp (cond-actions first))
+	            (sequence->exp (cond-actions first))
+	            (error "ELSE clause isn't last -- COND->IF" clauses))
+	        (make-if (cond-predicate first)
+                     (if (arrow-action? (cond-actions first))
+                         (list (cadr (cond-actions first))
+                               (cond-predicate first))
+                         (sequence->exp (cond-actions first)))
                      (expand-clauses rest))))))
 ;Value: expand-clauses
 
